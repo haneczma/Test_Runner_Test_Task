0d329ed8df25fa136a0bbec8e5508421414e5c5e
Hanna Zarzycka <01187305@pw.edu.pl>
2024-11-04 21:30:38 +0100

    Print verification of the response

--- B.py
+++ B.py
@@ -1,7 +1,8 @@
 from subprocess import PIPE, Popen
 
 
-def SendCommand(cmd_name: str) -> str:
+def SendCommand(p, cmd_name: str) -> str:
+    # sends command name to open process and returns stdout of that process
     p.stdin.write(cmd_name + "\n")
     p.stdin.flush()
     response = p.stdout.readline()
@@ -12,15 +13,17 @@ if __name__ == "__main__":
     # open program A as a process
     p = Popen(["python", "-u", "A.py"], stdin=PIPE, stdout=PIPE, text=True)
     # verify if Hi commannd gives a correct response
-    response = SendCommand("Hi")
+    response = SendCommand(p, "Hi")
     if response != "Hi":
-        raise AssertionError("Process A does not give the correct response.")
+        raise AssertionError("Program A does not give the correct response.")
+    else:
+        print("Program A gives a correct response.")
     # retrive 100 random integers
     random_data = list()
     for i in range(0, 100):
-        random_data.append(int(SendCommand("GetRandom")))
+        random_data.append(int(SendCommand(p, "GetRandom")))
     # terminate program A gracefully
-    print(SendCommand("Shutdown"))
+    print(SendCommand(p, "Shutdown"))
     # sort and print retrieved data
     random_data.sort()
     print(random_data)
